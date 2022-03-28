54160a90430a5db2be17ef9d270880984be1b58f
SteveWW <s@steveww.org>
2022-03-28 11:53:59 +0100

    check for repo

--- gitops/02-monitoring.sh
+++ gitops/02-monitoring.sh
@@ -7,6 +7,16 @@ set -e
 SCRIPTS="helm-prometheus-repo helm-prometheus kustomization-monitoring"
 SCRIPTPATH=$(dirname "$0")
 
+SCRIPT="git-repo"
+if [ ! -f "$SCRIPT" ]
+then
+    echo "$SCRIPT"
+    CMD="${SCRIPT}.sh"
+    YAML="${SCRIPT}.yaml"
+
+    "$SCRIPTPATH/scripts/$CMD" > "$YAML"
+fi
+
 for SCRIPT in $SCRIPTS
 do
     echo "$SCRIPT"

4d32b782fbdf97186c61d1130f00935d0aadf9e9
SteveWW <s@steveww.org>
2022-03-28 12:24:11 +0100

    handle repo

--- gitops/00-staging.sh
+++ gitops/00-staging.sh
@@ -7,6 +7,16 @@ set -e
 SCRIPTS="git-repo image-repo image-policy-staging image-auto-staging kustomization-staging"
 SCRIPTPATH=$(dirname "$0")
 
+SCRIPT="git-repo"
+CMD="${SCRIPT}.sh"
+YAML="${SCRIPT}.yaml"
+
+if [ ! -f "$YAML" ]
+then
+    echo "$SCRIPT"
+    "$SCRIPTPATH/scripts/$CMD" > "$YAML"
+fi
+
 for SCRIPT in $SCRIPTS
 do
     echo "$SCRIPT"
